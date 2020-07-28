b2dc442c93393c7c5142b44aa27ab5f284ce45bb
Wiktor Phillips <wsphillips@gmail.com>
2020-07-28 10:21:27 +0200

    .

--- src/LibUMP.jl
+++ src/LibUMP.jl
@@ -5,18 +5,15 @@ include("UMP/UMP.jl")
 using .UMP
 export UMP
 
-export get_position
+export Position
+export get_position, isbusy
+export move_X_by, move_Y_by, move_Z_by, #=move_VX_by,=# move_3D_by
 
-export move_X_by, move_Y_by, move_Z_by, move_VX_by, move_3D_by
-
-const DEF_IP = "169.254.255.255"
+const BROADCAST_IP = "169.254.255.255"
 global DEF_HANDLE = C_NULL
 const DEF_DEVICE = Cint(1)
 const MAX_TIMEOUT = Cuint(999)
-global DefaultUMP
-global UMP_STATE
 global UMP_NUMAXES
-const AXIS_ANGLE = 23.8
 
 mutable struct Position
     x::Cint
@@ -26,23 +23,18 @@ mutable struct Position
 end
 
 function __init__()
-    global DEF_HANDLE = UMP.open(DEF_IP, MAX_TIMEOUT, UMP.DEF_GROUP)
+    global DEF_HANDLE = UMP.open(BROADCAST_IP, MAX_TIMEOUT, UMP.DEF_GROUP)
     UMP.select_dev(DEF_HANDLE, DEF_DEVICE)
+    UMP.set_timeout(DEF_HANDLE, Cint(10)) # set UDP receive timeout to 10ms
     global UMP_NUMAXES = UMP.get_axis_count(DEF_HANDLE, DEF_DEVICE)
 end
 
 function flooraxis(val)
-    if val < 0
-        return 0
-    else
-        return val
-    end
+    return val < 0 ? 0 : val
 end
 
 function error_check(x)
-    if x < 0
-        @warn "Error return value: $x"
-    end
+    x < 0 && (@warn "Error return value: $x")
 end
 
 function get_position(num_axes = UMP_NUMAXES)
@@ -59,7 +51,7 @@ function get_position(num_axes = UMP_NUMAXES)
     end
 end 
 
-function poll_status(handle = DEF_HANDLE, timeout = 200)
+function poll_status(handle = DEF_HANDLE, timeout = 10)
     UMP.receive(handle, Cint(timeout))
     status::Cint = UMP.get_status(handle)
     return status
@@ -77,31 +69,26 @@ function moveto(pos::Position, speed::Integer = 5000)
     end
 end
 
-function moveto(pos::Position, xspeed, yspeed, zspeed, mode, accel, dev = DEF_DEVICE, handle = DEF_HANDLE)
-
+function moveto(pos::Position, xspeed, yspeed, zspeed, mode, accel; dev = DEF_DEVICE, handle = DEF_HANDLE)
     UMP.goto_position_ext2(handle, dev, pos.x, pos.y, pos.z, Cint(typemax(Cint)), Cint(xspeed), Cint(yspeed), Cint(zspeed),
                            Cint(0), Cint(mode), Cint(accel))
 end
 
 function take_step(xstep, ystep, zstep, xvel, yvel, zvel, dev = DEF_DEVICE, handle = DEF_HANDLE) end
 
-function move_3D_by(xstep, ystep, zstep, speed, handle = DEF_HANDLE)
-   
+function move_3D_by(xstep, ystep, zstep, speed; handle = DEF_HANDLE)
     UMP.take_step(handle, Cint(xstep), Cint(ystep), Cint(zstep), Cint(0), Cint(speed)) |> error_check
 end
 
-function move_X_by(xstep, speed, dev = DEF_DEVICE, handle = DEF_HANDLE)
-
+function move_X_by(xstep, speed; handle = DEF_HANDLE)
     UMP.take_step(handle, Cint(xstep), Cint(0), Cint(0), Cint(0), Cint(speed))
 end
 
 function move_Y_by(ystep, speed, dev = DEF_DEVICE, handle = DEF_HANDLE)
-
     UMP.take_step(handle, Cint(0), Cint(ystep), Cint(0), Cint(0), Cint(speed))
 end
 
 function move_Z_by(zstep, speed, dev = DEF_DEVICE, handle = DEF_HANDLE)
-
     UMP.take_step(handle, Cint(0), Cint(0), Cint(zstep), Cint(0), Cint(speed))
 end
 
